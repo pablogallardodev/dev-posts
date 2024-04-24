2b0a8f9a880b37be6e1b9cfc7b31d97e25c45fb9
Pablo Gallardo <38090176+pablogallardodev@users.noreply.github.com>
2024-04-23 23:30:28 -0600

    Delete Credentials

--- src/Firebase/client.js
+++ src/Firebase/client.js
@@ -2,14 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 
 const PROVIDER_GOOGLE = new GoogleAuthProvider();
-const firebaseConfig = {
-  apiKey: "AIzaSyDaxoQAe5x085XR6dcekSxP5i3vj2DgwVA",
-  authDomain: "blog-fc938.firebaseapp.com",
-  projectId: "blog-fc938",
-  storageBucket: "blog-fc938.appspot.com",
-  messagingSenderId: "117389127485",
-  appId: "1:117389127485:web:2e439e2a371686f16e5f54"
-}
+const firebaseConfig = {}
 
 initializeApp(firebaseConfig)
 
