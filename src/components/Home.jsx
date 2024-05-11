e941cdd38c6275f276b35ed05220731f9ee0596a
pablogallardodev <pablito.260197@gmail.com>
2024-05-10 18:06:15 -0600

    React 69: Listando post desde archivo JSON

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,33 @@
 import { onSignOut } from '../Firebase/client'
 import useUsuairo from '../hooks/useUsuario'
+import posts from '../Firebase/post.json'
+import styles from '../styles/home.module.css'
 
 const Home = () => {
   const usuario = useUsuairo()
 
   return (
-    <nav>
-      <h1>{usuario}</h1>
-      <button onClick={ onSignOut }>Cerrar sesión</button>
-    </nav>
+    <div className={styles.container}>
+      <nav>
+        <h1>{usuario}</h1>
+        <button onClick={ onSignOut }>Cerrar sesión</button>
+      </nav>
+      <div>
+        {
+          posts.map( post =>
+            <section key={post.id} className={styles.containerPost}>
+              <p>{post.user_name} {post.creation_date}</p>
+              <p>{post.post_message}</p>
+              <img src={post.post_img} alt={post.id} />
+              <br />
+              <span>Reacciones: {post.reactions}</span>
+              <br />
+              <span>Comentarios: {post.comments.length}</span>
+            </section>
+          )
+        }
+      </div>
+    </div>
   )
 }
 
