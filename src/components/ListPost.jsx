87a4de8d870c068313df6955bc5e2e07f8f6cb0b
pablogallardodev <pablito.260197@gmail.com>
2024-11-07 20:41:43 -0600

    React 76: Rediseño de post

--- src/components/ListPost.jsx
+++ src/components/ListPost.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import styles from '../styles/home.module.css'
+import styles from '../styles/post.module.css'
 import { getAllPosts } from '../Firebase/database'
+import heart from '../img/icons/heart.svg'
+import comments from '../img/icons/comments.svg'
 
 const ListPost = () => {
   const [posts, setPosts] = useState([])
@@ -17,15 +19,21 @@ const ListPost = () => {
   }, [])
 
   return posts.map( post =>
-    <section key={post.id} className={styles.containerPost}>
-      <p>{post.user_name} {post.creation_date}</p>
-      <p>{post.post_message}</p>
-      <img src={post.post_img} alt={post.id} />
-      <br />
-      <span>Reacciones: {post.reactions}</span>
-      <br />
-      <span>Comentarios: {post.comments.length}</span>
-    </section>
+    <article key={post.id} className={styles.containerPost}>
+      <section className={styles.sectionTitle}>
+        <p>{post.user_name}</p>
+        <p>{post.creation_date}</p>
+      </section>
+      
+      <p className={post.post_img ? styles.message : styles.textImage}>{post.post_message}</p>
+
+      {post.post_img && <img src={post.post_img} alt={post.id} />}
+
+      <div className={styles.containerButtons}>
+        <button><img src={heart} width={16}/>{post.reactions}</button>
+        <button><img src={comments} width={16}/>{post.comments.length}</button>
+      </div>
+    </article>
   )
 }
 
