ab243780516e302a7e76a7657d07c369d6b5951f
pablogallardodev <pablito.260197@gmail.com>
2024-11-18 17:52:51 -0600

    React 77: Rediseño de tarjeta usuario

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,19 @@
-import { onSignOut } from '../Firebase/auth'
-import useUsuairo from '../hooks/useUsuario'
 import styles from '../styles/home.module.css'
 import AddPost from './AddPost'
+import CardUser from './CardUser'
 import ListPost from './ListPost'
 
 const Home = () => {
-  const usuario = useUsuairo()
   
   return (
     <div className={styles.container}>
       <nav>
-        <h1>{usuario?.email}</h1>
-        <button onClick={ onSignOut }>Cerrar sesión</button>
+        <CardUser />
       </nav>
-      <div>
+      <section>
         <AddPost />
         <ListPost />
-      </div>
+      </section>
     </div>
   )
 }
